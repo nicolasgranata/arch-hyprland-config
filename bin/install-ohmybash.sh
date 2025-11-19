aca22057d10f9361c0c216b55062a6c949cfab39
Nicolas Granata <nicolasegranata@gmail.com>
2025-11-19 19:38:09 -0300

    Added silent option to ohmybash

--- bin/install-ohmybash.sh
+++ bin/install-ohmybash.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "Instalando Oh My Bash"
-bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)" -s
