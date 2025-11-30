7f03c706d05daaa6b2a7284d44649bc8ea9eae4a
Nicolas Granata <nicolasegranata@gmail.com>
2025-11-29 22:27:41 -0300

    Add utility file

---
+++ bin/install-utilities.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Instalando USB Mode Switch"
+sudo pacman -S --needed --noconfirm usb_modeswitch
+
+echo "Instalando 7zip"
+sudo pacman -S --needed --noconfirm 7zip
+
+echo "Instalando Trash Cli"
+sudo pacman -S --needed --noconfirm trash-cli
